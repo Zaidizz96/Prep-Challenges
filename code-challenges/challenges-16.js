e0cc4c986ea41d6a3a6f471d409a5c7ccfbf9701
Zaidizz96 <Zaid.ali.izziddine@gmail.com>
2023-07-10 20:40:30 +0000

    exercise16

---
+++ code-challenges/challenges-16.js
@@ -0,0 +1,103 @@
+'use strict';
+
+
+// Combine methods
+
+// -------------------------------------------------------------------------------------------------------
+// Challenge 01:
+// Required:
+//
+// Write a function that takes in a string and returns the reverse of this string .
+//
+// Input <= "Hello";
+// Output => "olleH";
+
+const reverseString = (string) => {
+    //write your code here ...
+    // there's reserved function in java script string.split("").reverse().join("") ,
+    // but i try to use my logic to resolve the challange.
+
+    var strArray = string.split("");
+    let newStr = '';
+
+    for (var i = strArray.length - 1; i >= 0; i--) { 
+        newStr += strArray[i]; 
+    }
+   return newStr
+}
+// -------------------------------------------------------------------------------------------------------
+
+
+
+// -------------------------------------------------------------------------------------------------------
+// Challenge 02:
+// Required:
+//
+// Write a function that takes an array of strings and returns only strings that contain ^_^.
+//
+// Ex :-
+// Input <= ["hello ^_^ ","Hi ^_^" ,"What's up ^_-" ,"lol"] , Output => ["hello ^_^ ","Hi ^_^" ] ;
+
+const detectFace = (arr) => {
+    //write your code here ...
+    let regex=/([\^][_][\^])/
+
+    var newArray = arr.filter((obj) => {
+        return regex.test(obj);
+    });
+
+    return newArray;
+}
+// -------------------------------------------------------------------------------------------------------
+
+
+// -------------------------------------------------------------------------------------------------------
+// Challenge 03:
+// Optional:
+//
+// Write a function that takes in a string and returns a string contains only characters in even positions.
+//
+// Ex :-
+// Input <= "coding" output =>"cdn"
+
+const eveCharacter = (str) => {
+    //write your code here ...
+   let newArr = str.split("");
+   let newStr = '';
+   
+   newArr.forEach((element,index) => {
+    if ((index % 2) == 0){
+        newStr += element;
+    }
+   });
+
+   return newStr;
+}
+// -------------------------------------------------------------------------------------------------------
+
+
+// -------------------------------------------------------------------------------------------------------
+// Challenge 04
+// Optional:
+
+// Let us consider you are hired as a software developer in a tech company and you are assigned to work on a restaurant web application project .
+
+// And you are assigned to write function to return only ingredients that contains chicken.
+//
+// Input:
+// [["mushroom", "grilled chicken", "sauce"], ["Bread", "Potato", "baked chicken"], ["fried potato", "garlic sauce", "fried chicken"]];
+//
+// Output:
+// [["grilled chicken"], ["baked chicken"], ["fried chicken"]];
+
+
+const chickenGradients = (arr) => {
+    //write your code here ...
+    let newArr = arr.forEach(obj => {
+        obj.forEach(nestObj => (nestObj.contains('chicken')));
+    })
+    return newArr;
+}
+// -------------------------------------------------------------------------------------------------------
+
+module.exports = { reverseString, detectFace, eveCharacter, chickenGradients };
